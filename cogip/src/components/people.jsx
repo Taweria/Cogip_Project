840550b383f03a92117f0bfd664427c9e64d3aca
Taweria <elodieali.pro@gmail.com>
2023-12-05 16:43:11 +0100

    Change font

--- cogip/src/components/people.jsx
+++ cogip/src/components/people.jsx
@@ -14,8 +14,8 @@ const People = () => {
          <div className="bg-slate-100 w-11/12 rounded-xl flex flex-col justify-center items-center my-9 mx-auto md:mx-0 md:flex-row md:gap-8 md:w-2/12">
             <img src={avatar} alt="avatar" className="rounded-full mt-10 md:mt-0" />
             <div className="flex gap-2 my-8 md:flex-col md:gap-0">
-                <p className="text-xl font-black">John</p>
-                <p className="text-xl font-black">Doe</p>
+                <p className="text-xl font-black font-roboto">John</p>
+                <p className="text-xl font-black font-roboto">Doe</p>
             </div>
          </div>
     )
