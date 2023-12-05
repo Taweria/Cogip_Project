fbaefbc5a3556f83a3c91fb35ca109bae71a7963
Alexandre <vensalex1991@gmail.com>
2023-12-05 22:10:25 +0100

    maxwidth table.jsx in %

--- cogip/src/components/table.jsx
+++ cogip/src/components/table.jsx
@@ -3,7 +3,7 @@ import { Column } from 'primereact/column';
 
 const Table = () => {
     return (
-        <DataTable tableStyle={{ maxWidth: '50rem' }}>
+        <DataTable tableStyle={{ maxWidth: '50%' }}>
             <Column field="" header="Invoice number"></Column>
             <Column field="" header="Dates due"></Column>
             <Column field="" header="Company"></Column>
