cbf2a421b6ddec1b4fcbc2b9ea9f4e9222ad1ca4
Taweria <elodieali.pro@gmail.com>
2023-12-05 16:49:58 +0100

    Add padding

--- cogip/src/layouts/contactPeople.jsx
+++ cogip/src/layouts/contactPeople.jsx
@@ -2,7 +2,7 @@ import People from "../components/people";
 
 const ContactPeople = () => {
     return (
-         <div className="border-y-2 divide-solid border-slate-100 w-11/12 mx-auto">
+         <div className="border-y-2 divide-solid border-slate-100 w-11/12 mx-auto py-12">
             <h2 className="text-6xl font-black font-inter">Contact People</h2>
             <div className="flex flex-col gap-4 md:flex-row">
                 <People /> <People/>
