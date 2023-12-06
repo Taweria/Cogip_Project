8756514a28103d52859c2edd1e1651bde251efd6
Taweria <elodieali.pro@gmail.com>
2023-12-06 11:10:01 +0100

    Avatars + generated random names

--- cogip/src/App.jsx
+++ cogip/src/App.jsx
@@ -5,7 +5,6 @@ import WorkBetter from "./components/workbetter.jsx";
 import LastInvoices from "./components/lastInvoices.jsx";
 import LastContacts from "./components/lastContacts.jsx";
 import LastCompanies from "./components/lastCompanies.jsx";
-import ContactPeople from "./layouts/contactPeople.jsx";
 
 
 const router = createBrowserRouter([
@@ -18,7 +17,6 @@ const router = createBrowserRouter([
                 <LastContacts />
                 <LastCompanies />
                 <WorkBetter />
-                <ContactPeople />
                 <Footer />
             </>
         ),
