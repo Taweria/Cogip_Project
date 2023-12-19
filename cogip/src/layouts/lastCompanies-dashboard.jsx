0fe2316ab4e32b65f3aafc7de572d82eb1a4cad7
Taweria <elodieali.pro@gmail.com>
2023-12-19 13:33:41 +0100

    IDK

--- cogip/src/layouts/lastCompanies-dashboard.jsx
+++ cogip/src/layouts/lastCompanies-dashboard.jsx
@@ -20,7 +20,7 @@ const LastCompaniesDashboard = () => {
     return (
         <div className="w-full md:w-11/12 mt-16 p-4 rounded-md font-inter shadow-md shadow-gray-200 bg-white">
             <h2 className="font-inter mx-6 mb-6 font-bold text-xl border-b-2 border-slate-100 pb-4"> Last Companies </h2>
-            <TableDashboard data={companies} loading={loading} tableKey={tableKey} tableHead={tableHead} link={`newcompany`}/>
+            <TableDashboard data={companies} loading={loading} tableKey={tableKey} tableHead={tableHead} link={`new-company`}/>
         </div>
     )
 }
